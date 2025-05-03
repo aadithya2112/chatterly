92c6bbdbb2a9a8574756b538a76b75c3b27d0b6e
aadithya <aadithyacps@gmail.com>
2025-05-03 17:19:10 +0530

    add production urls

--- apps/chat-widget/src/config.ts
+++ apps/chat-widget/src/config.ts
@@ -1,2 +1,6 @@
-export const HTTP_BACKEND_URL = "http://localhost:3000/api";
-export const WS_BACKEND_URL = "http://localhost:4000";
+// export const HTTP_BACKEND_URL = "http://localhost:3000/api"; // For development only
+// export const WS_BACKEND_URL = "http://localhost:4000"; // For development only
+
+// Production URLs
+export const WS_BACKEND_URL = "https://chatterly-wss.aadithya.tech";
+export const HTTP_BACKEND_URL = "https://chatterly.aadithya.tech/api";
